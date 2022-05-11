aac86707f19636c0cdd9f0bd4d4c3b6c2e600649
Daniel Ceballos <dceballosm@unal.edu.co>
2022-05-10 23:52:55 -0500

    Create Camion.java

---
+++ src/main/java/vehiculos/Camion.java
@@ -0,0 +1,19 @@
+package vehiculos;
+
+public class Camion extends Vehiculo{
+	protected int ejes;
+	private static int cantidadCamiones = 0;
+	
+	public Camion(String placa, String nombre, int precio, int peso, Fabricante fabricante, int ejes) {
+		this(placa, 2, 80, nombre, precio, peso, "4X2", fabricante, ejes);
+	}
+	public Camion(String placa, int puertas, int velocidadMaxima, String nombre, int precio, int peso, String traccion, Fabricante fabricante, int ejes){
+		super(placa, puertas, velocidadMaxima, nombre, precio, peso, traccion, fabricante);
+		this.ejes = ejes;	
+		Camion.cantidadCamiones = Camion.cantidadCamiones + 1;
+	}
+	public int getEjes() {return this.ejes;}
+	public static int getCantidadCamiones() {return Camion.cantidadCamiones;}
+	
+	public void setEjes(int ejes) {this.ejes = ejes;}
+}
