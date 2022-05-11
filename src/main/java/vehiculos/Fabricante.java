36624f222667ca05cd0537f5bb95cc64ef158e00
Daniel Ceballos <dceballosm@unal.edu.co>
2022-05-11 00:08:58 -0500

    Update Fabricante.java

--- src/main/java/vehiculos/Fabricante.java
+++ src/main/java/vehiculos/Fabricante.java
@@ -1,43 +1,42 @@
 package vehiculos;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Map.Entry;
+import java.util.ArrayList;
 
 public class Fabricante {
 	private String nombre;
 	private Pais pais;
-	static Map<Fabricante, Integer> ventasPorFabrica = new HashMap <Fabricante, Integer>();
+	private static ArrayList<Fabricante> fabricantes = new ArrayList<Fabricante>();
+	private int contador;
 	
-	public static Fabricante fabricaMayorVentas()
-	{
-		int valorMax = -1;
-		Fabricante fabricaMayorVentas = null;
-
-		for (Entry<Fabricante, Integer> entry : ventasPorFabrica.entrySet()) {
-		    final int valorActual = entry.getValue();
-
-		    if (valorActual > valorMax) 
-		    {
-		    	valorMax = valorActual;
-		    	fabricaMayorVentas = entry.getKey();
-		    } 	   
-		}
-		
-		return fabricaMayorVentas;
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
+	public Fabricante(String nombre, Pais pais) {
 		this.nombre = nombre;
-	}
-	public Pais getPais() {
-		return pais;
-	}
-	public void setPais(Pais pais) {
 		this.pais = pais;
+		contador = 0;
+		fabricantes.add(this);
 	}
+	
+	public String getNombre() {return nombre;}
+	
+	public Pais getPais() {return pais;}
+	
+	public static ArrayList<Fabricante> getFabricantes(){return fabricantes;}
+	
+	public int getContador() {return contador;}
+	
+	public void setNombre(String nombre) {this.nombre = nombre;}
+	
+	public void setPais(Pais pais) {this.pais = pais;}
+	
+	public void setContador() {contador++;}
+	
+	public static Fabricante fabricaMayorVentas() {
+		Fabricante summit = fabricantes.get(0);
+		for(int i = 1; i < fabricantes.size(); i++) {
+			if(fabricantes.get(i).getContador() > summit.getContador()) {
+				summit = fabricantes.get(i);
+			}
+		}
+		return summit;
+	}
+	
 
 }
