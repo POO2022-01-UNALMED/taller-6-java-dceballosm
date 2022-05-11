f7a10db91deea7083022f22d9f83fd8270a3719d
Daniel Ceballos <dceballosm@unal.edu.co>
2022-05-11 00:19:09 -0500

    Update Vehiculo.java

--- src/main/java/vehiculos/Vehiculo.java
+++ src/main/java/vehiculos/Vehiculo.java
@@ -6,12 +6,13 @@ public class Vehiculo {
 	protected int velocidadMaxima;
 	protected String nombre;
 	protected int precio;
-	protected int peso;
+	protected float peso;
 	protected String traccion;
 	protected Fabricante fabricante;
 	private static int CantidadVehiculos = 0;
 	
-	public Vehiculo(String placa, int puertas, int velocidadMaxima, String nombre, int precio, int peso, String traccion, Fabricante fabricante) {
+	public Vehiculo (String placa, int puertas, int velocidadMaxima, String nombre, int precio, float peso,
+			String traccion, Fabricante fabricante) {
 		this.placa = placa;
 		this.puertas = puertas;
 		this.velocidadMaxima = velocidadMaxima;
@@ -20,7 +21,7 @@ public class Vehiculo {
 		this.peso = peso;
 		this.fabricante = fabricante;
 		this.traccion = traccion;
-		CantidadVehiculos ++;
+		CantidadVehiculos++;
 		fabricante.setContador();
 		fabricante.getPais().setContador();
 		Fabricante.getFabricantes().add(fabricante);
@@ -32,7 +33,7 @@ public class Vehiculo {
 	public int getVelocidadMaxima() {return this.velocidadMaxima;}
 	public String getNombre() {return this.nombre;} 
 	public int getPrecio() {return this.precio;}
-	public int getPeso() {return this.peso;}
+	public float getPeso() {return this.peso;}
 	public Fabricante getFabricante() {return this.fabricante;}
 	public String getTraccion() {return this.traccion;}
 	public int getCantidadVehiculos() {return Vehiculo.CantidadVehiculos;}
@@ -43,7 +44,7 @@ public class Vehiculo {
 	public void setVelocidadMaxima(int velocidadMaxima) {this.velocidadMaxima = velocidadMaxima;}
 	public void setNombre(String nombre) {this.nombre = nombre;} 
 	public void setPrecio(int precio) {this.precio = precio;}
-	public void setPeso(int peso) {this.peso = peso;}
+	public void setPeso(float peso) {this.peso = peso;}
 	public void setFabricante(Fabricante fabricante) {this.fabricante = fabricante;}
 	public void setTraccion(String traccion) {this.traccion = traccion;}
 	public static void setCantidadVehiculos(int CantidadVehiculos) {Vehiculo.CantidadVehiculos=CantidadVehiculos;}
