2019c1430254d717675ecfc0ce67f48945ff2452
Daniel Ceballos <dceballosm@unal.edu.co>
2022-05-10 23:52:57 -0500

    Create Camioneta.java

---
+++ src/main/java/vehiculos/Camioneta.java
@@ -0,0 +1,19 @@
+package vehiculos;
+
+public class Camioneta extends Vehiculo{
+	protected boolean volco;
+	private static int cantidadCamionetas = 0;
+	
+	public Camioneta(String placa, int puertas, String nombre, int precio, int peso, Fabricante fabricante, boolean volco){
+		this(placa, puertas, 90, nombre, precio, peso, "4X", fabricante, volco);	
+	}
+	public Camioneta(String placa, int puertas, int velocidadMaxima, String nombre, int precio, int peso, String traccion, Fabricante fabricante, boolean volco){
+		super(placa, puertas, velocidadMaxima, nombre, precio, peso, traccion, fabricante);
+		this.volco = volco;
+		Camioneta.cantidadCamionetas = Camioneta.cantidadCamionetas + 1;
+	}
+	public boolean isVolco() {return this.volco;}
+	public static int getCantidadCamionetas() {return Camioneta.cantidadCamionetas;}
+	
+	public void setVolco(boolean volco) {this.volco = volco;}
+}
