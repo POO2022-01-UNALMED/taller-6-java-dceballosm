97066cccd66212728276bcacfa05b23a45eb8800
Daniel Ceballos <dceballosm@unal.edu.co>
2022-05-11 00:19:01 -0500

    Update Automovil.java

--- src/main/java/vehiculos/Automovil.java
+++ src/main/java/vehiculos/Automovil.java
@@ -4,14 +4,13 @@ public class Automovil extends Vehiculo{
 	protected int puestos;
 	private static int cantidadAutomoviles=0;
 	
-	public Automovil(String placa, String nombre, int precio, int peso, Fabricante fabricante, int puestos) {
-		this(placa, 4, 100, nombre, precio, peso, "FWD", fabricante, puestos);
-	}
-	
-	public Automovil(String placa, int puertas, int velocidadMaxima, String nombre, int precio, int peso, String traccion, Fabricante fabricante, int puestos) {
-		super(placa, puertas, velocidadMaxima, nombre, precio, peso, traccion, fabricante);
+	public Automovil(String placa, String nombre, int precio, float peso,
+			Fabricante fabricante, int puestos) {
+		super(placa, 4, 100, nombre, precio, peso, "FWD", fabricante);
+		
 		this.puestos = puestos;
-		cantidadAutomoviles ++;
+		cantidadAutomoviles++;
+		
 	}
 
 	public int getPuestos() {return this.puestos;}
