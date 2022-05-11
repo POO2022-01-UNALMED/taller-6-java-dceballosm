41651c73044d4c2864e9fd85b4f7a7bc971846fc
Daniel Ceballos <dceballosm@unal.edu.co>
2022-05-10 23:57:14 -0500

    Create Automovil.java

---
+++ src/main/java/vehiculos/Automovil.java
@@ -0,0 +1,21 @@
+package vehiculos;
+
+public class Automovil extends Vehiculo{
+	protected int puestos;
+	private static int cantidadAutomoviles=0;
+	
+	public Automovil(String placa, String nombre, int precio, int peso, Fabricante fabricante, int puestos) {
+		this(placa, 4, 100, nombre, precio, peso, "FWD", fabricante, puestos);
+	}
+	
+	public Automovil(String placa, int puertas, int velocidadMaxima, String nombre, int precio, int peso, String traccion, Fabricante fabricante, int puestos) {
+		super(placa, puertas, velocidadMaxima, nombre, precio, peso, traccion, fabricante);
+		this.puestos = puestos;
+		Automovil.cantidadAutomoviles = Automovil.cantidadAutomoviles + 1;
+	}
+
+	public int getPuestos() {return this.puestos;}
+	public static int getCantidadAutomoviles() {return Automovil.cantidadAutomoviles;}
+	
+	public void setPuestos(int puestos) {this.puestos = puestos;}
+}
