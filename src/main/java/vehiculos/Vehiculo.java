b91fbbd55c02d244df1995f57e6c4a4396285f0d
Daniel Ceballos <dceballosm@unal.edu.co>
2022-05-11 00:21:24 -0500

    Update Vehiculo.java

--- src/main/java/vehiculos/Vehiculo.java
+++ src/main/java/vehiculos/Vehiculo.java
@@ -1,15 +1,17 @@
 package vehiculos;
 
+
 public class Vehiculo {
-	protected String placa;
-	protected int puertas;
-	protected int velocidadMaxima;
-	protected String nombre;
-	protected int precio;
+	protected String placa; 
+	protected int puertas; 
+	protected int velocidadMaxima; 
+	protected String nombre; 
+	protected int precio; 
 	protected float peso;
-	protected String traccion;
+	protected String traccion; 
 	protected Fabricante fabricante;
-	private static int CantidadVehiculos = 0;
+	
+	private static int vehiculos = 0;
 	
 	public Vehiculo (String placa, int puertas, int velocidadMaxima, String nombre, int precio, float peso,
 			String traccion, Fabricante fabricante) {
@@ -19,37 +21,46 @@ public class Vehiculo {
 		this.nombre = nombre;
 		this.precio = precio;
 		this.peso = peso;
-		this.fabricante = fabricante;
 		this.traccion = traccion;
-		CantidadVehiculos++;
+		this.fabricante = fabricante;
+		vehiculos++;
 		fabricante.setContador();
 		fabricante.getPais().setContador();
 		Fabricante.getFabricantes().add(fabricante);
 		Pais.getPaises().add(fabricante.getPais());
 	}
 	
-	public String getPlaca() {return this.placa;}
-	public int getPuertas() {return this.puertas;}
-	public int getVelocidadMaxima() {return this.velocidadMaxima;}
-	public String getNombre() {return this.nombre;} 
-	public int getPrecio() {return this.precio;}
-	public float getPeso() {return this.peso;}
-	public Fabricante getFabricante() {return this.fabricante;}
-	public String getTraccion() {return this.traccion;}
-	public int getCantidadVehiculos() {return Vehiculo.CantidadVehiculos;}
-
 	
-	public void setPlaca(String placa) {this.placa=placa;}
+	
+	public String getPlaca() {return placa;}
+	public void setPlaca(String placa) {this.placa = placa;}
+	
+	public int getPuertas() {return puertas;}
 	public void setPuertas(int puertas) {this.puertas = puertas;}
+	
+	public float getVelocidadMaxima() {return velocidadMaxima;}
 	public void setVelocidadMaxima(int velocidadMaxima) {this.velocidadMaxima = velocidadMaxima;}
-	public void setNombre(String nombre) {this.nombre = nombre;} 
+
+	public String getNombre() {return nombre;}
+	public void setNombre(String nombre) {this.nombre = nombre;}
+	
+	public float getPrecio() {return precio;}
 	public void setPrecio(int precio) {this.precio = precio;}
+	
+	public float getPeso() {return peso;}
 	public void setPeso(float peso) {this.peso = peso;}
-	public void setFabricante(Fabricante fabricante) {this.fabricante = fabricante;}
+	
+	public String getTraccion() {return traccion;}
 	public void setTraccion(String traccion) {this.traccion = traccion;}
-	public static void setCantidadVehiculos(int CantidadVehiculos) {Vehiculo.CantidadVehiculos=CantidadVehiculos;}
+	
+	public Fabricante getFabricante() {return fabricante;}
+	public void setFabricante(Fabricante fabricante) {this.fabricante = fabricante;}
+	
+	public static int getCantidadVehiculos(){return vehiculos;}
+	public static void setCantidadVehiculos(int vehiculos) {Vehiculo.vehiculos=vehiculos;}
 	
 	public String vehiculosPorTipo() {
-		return "Automoviles: " + Automovil.getCantidadAutomoviles() + "\nCamionetas: " + Camioneta.getCantidadCamionetas() + "\nCamiones: " + Camion.getCantidadCamiones();
+		return "Automoviles: " + Automovil.getAutomoviles() + "\nCamionetas: " + Camioneta.getCamionetas() + "\nCamiones: " + Camion.getCamiones();
 	}
+	
 }
